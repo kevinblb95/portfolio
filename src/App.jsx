213421f1dd369513f1dc4f8fe029106b4483d317
kbalubam <you@example.com>
2022-01-04 14:16:31 +0100

    bases

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Intro from "./components/intro/Intro";
+import Topbar from "./components/topbar/Topbar";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonial from "./components/testimonial/Testimonial";
+import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar/>
+     <div className="sections">
+      <Intro/>
+      <Portfolio/>
+      <Works/>
+      <Testimonial/>
+      <Contact/>
+
+     </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,11 @@
+import "./testimonial.scss"
+
+const Testimonial = () => {
+    return (
+        <div className="testimonial">
+            
+        </div>
+    )
+}
+
+export default Testimonial
