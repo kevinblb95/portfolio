c98044e5fa49e78ae6a0a6ececbc3304bb31dfc2
kbalubam <you@example.com>
2022-01-11 17:26:02 +0100

    portfolio active

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,66 @@
+import { useState } from "react"
+import PortfolioList from "../portfolioList/PortfolioList"
 import "./portfolio.scss"
 
 const Portfolio = () => {
+
+    const [selected, setSelected] = useState("featured")
+    const list = [
+        {
+            id: "featured",
+            title: "Featured"
+        },
+        {
+            id: "web",
+            title: "Web App"
+        },
+        {
+            id: "mobile",
+            title: "Mobile App"
+        },
+        {
+            id: "design",
+            title: "Design"
+        },
+        {
+            id: "content",
+            title: "Content"
+        },
+    ]
     return (
         <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map(item=>(
+                    <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
+                ))}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/image_port.jpg" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/image_port.jpg" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/image_port.jpg" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/image_port.jpg" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/image_port.jpg" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/image_port.jpg" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+            </div>
             
         </div>
     )
---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './portfolioList.scss';
+
+const portfolioList = ({id, title, active, setSelected}) => {
+    return (
+        <li className={active ? "portfolioList active" : "portfolioList"} onClick={()=>setSelected(id)}>
+            {title}
+        </li>
+    )
+}
+
+export default portfolioList
