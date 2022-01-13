b7b8baed57ece6e24b5b5e62ffba0a99ad741d01
kbalubam <you@example.com>
2022-01-13 13:51:22 +0100

    slider intro

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,33 @@ import "./works.scss"
 const Works = () => {
     return (
         <div className="works" id="works">
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/mobile1.png" alt="" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita ratione deserunt perferendis reiciendis rem adipisci dicta at corporis consequatur. 
+                                </p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right ">
+                            <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930" />
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <img src="assets/white-arrow.png" className="arrow left" alt="" />
+            <img src="assets/white-arrow.png" className="arrow right" alt="" />
+
         </div>
     )
 }
